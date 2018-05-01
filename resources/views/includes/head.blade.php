8d31283b6fb6a4ccfe196a3f96d27dcf2b4441fe
Creighton Magoun <magoun@gmail.com>
2018-04-30 22:45:58 -0500

    Tinker with page icon

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,7 +2,7 @@
 
 <meta charset="utf-8" />
 <link rel="apple-touch-icon" sizes="76x76" href="../resources/assets/img/apple-icon.png">
-<link rel="icon" type="image/png" href="../resources/assets/img/favicon.png">
+<link rel="icon" type="image/png" href="../resources/assets/img/now-logo.png">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
 
