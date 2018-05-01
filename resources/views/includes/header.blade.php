90b68ec7ffbfdc45bad4c328a44c98e58d3b2677
Creighton Magoun <magoun@gmail.com>
2018-04-30 22:46:25 -0500

    Continue customizing navigation bar with Now UI Kit

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,28 +3,30 @@
     <div class="container">
         <div class="navbar-translate">
             <a class="navbar-brand" href="https://sccodes.org/" rel="tooltip" title="LEARN PROGRAMMING. BUILD THE [FUTURE];" data-placement="bottom" target="_blank">
-                SC Codes
+                <i class="fa fa-graduation-cap"></i>
+                <span>SC Codes</span>
             </a>
             
-            <!--
-                Altered navigation bar code above.
-                Unaltered navigation bar from index.blade.php below.
-            -->
-            
+            <!-- I think this makes a menu bar on small screens. Not sure. -->
             <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-bar bar1"></span>
                 <span class="navbar-toggler-bar bar2"></span>
                 <span class="navbar-toggler-bar bar3"></span>
             </button>
         </div>
+        
+            <!--
+                Altered navigation bar code above.
+                Unaltered navigation bar from index.blade.php below.
+            -->
         <div class="collapse navbar-collapse justify-content-end" data-nav-image="../resources/assets/img/blurred-image-1.jpg">
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0)" onclick="scrollToDownload()">
-                        <i class="now-ui-icons arrows-1_cloud-download-93"></i>
-                        <p>Download</p>
-                    </a>
-                </li>
+                <!--<li class="nav-item">-->
+                <!--    <a class="nav-link" href="javascript:void(0)" onclick="scrollToDownload()">-->
+                <!--        <i class="now-ui-icons arrows-1_cloud-download-93"></i>-->
+                <!--        <p>Download</p>-->
+                <!--    </a>-->
+                <!--</li>-->
                 <li class="nav-item">
                     <a class="nav-link" href="./documentation/tutorial-components.html" target="_blank">
                         <i class="now-ui-icons files_paper"></i>
