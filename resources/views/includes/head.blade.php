ad91f13f3d57c85fda4a85f207e013ecd69d2295
Creighton Magoun <magoun@gmail.com>
2018-05-06 20:31:21 -0500

    Update head to include js files and new css file locations

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,8 +1,8 @@
 <html lang="en">
 
 <meta charset="utf-8" />
-<link rel="apple-touch-icon" sizes="76x76" href="../resources/assets/img/apple-icon.png">
-<link rel="icon" type="image/png" href="../resources/assets/img/now-logo.png">
+<link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon.png">
+<link rel="icon" type="image/png" href="img/now-logo.png">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
 
@@ -11,8 +11,35 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
 
 <!-- CSS Files -->
-<link href="../resources/assets/css/bootstrap.min.css" rel="stylesheet" />
-<link href="../resources/assets/css/now-ui-kit.css?v=1.1.0" rel="stylesheet" />
-<link href="../resources/assets/css/style.css" rel="stylesheet" />
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/now-ui-kit.css?v=1.1.0" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
+
+<!--   Core JS Files   -->
+<script src="js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
+<script src="js/core/popper.min.js" type="text/javascript"></script>
+<script src="js/core/bootstrap.min.js" type="text/javascript"></script>
+
+<!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+<script src="js/plugins/bootstrap-switch.js"></script>
+
+<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+<script src="js/plugins/nouislider.min.js" type="text/javascript"></script>
+
+<!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+<script src="js/plugins/bootstrap-datepicker.js" type="text/javascript"></script>
+
+<!-- Share Library etc -->
+<script src="js/plugins/jquery.sharrre.js" type="text/javascript"></script>
+
+<!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
+<script src="js/now-ui-kit.js?v=1.1.0" type="text/javascript"></script>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        // the body of this function is in assets/js/now-ui-kit.js
+        nowuiKit.initSliders();
+    });
+</script>
 
 <title>@yield('title')</title>
