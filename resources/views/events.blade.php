d5514b66f8a69cf90fdc5c129c541b7f73adee23
Creighton Magoun <magoun@gmail.com>
2018-05-22 22:19:44 -0500

    Updates for html validation

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -7,22 +7,21 @@
 	<h3>Choose a month to filter</h3>
 	
 	<form>
-    <select class="form-control" name="month" >
+    <select class="form-control" name="month">
 	    @foreach( $months as $month )
-	      <option value="<?= $month ?>"><?= $month ?></option>
-	      <br/>
+	      <option value="<?= $month ?>">
+	      	<?= $month ?>
+	      </option>
 	    @endforeach
     </select>
     
     <button class="btn btn-primary btn-round" type="submit">Submit</button>
-  	<a href="<?= explode("?", $_SERVER['REQUEST_URI'])[0] ?>">
-    	<button class="btn btn-primary btn-round btn-simple" type="button">
+  	<a href="<?= explode("?", $_SERVER['REQUEST_URI'])[0] ?>" class="btn btn-primary btn-round btn-simple" role="button">
     		Clear filter
-    	</button>
     </a>
   </form>
 	
-	<br />
+	<br>
 	
 	<ul>
 		@foreach( $events as $event )
@@ -31,7 +30,7 @@
 			    {{ $event->event_name }} hosted by {{ $event->group_name }}
 		    </strong>
 		    
-		    <a href=<?= build_cal_url( $event ); ?> target="_blank">
+		    <a href="<?= build_cal_url( $event ); ?>" target="_blank">
   				<ul>
   					<li><strong>Time:</strong> <?= DateTime::
   					  createFromFormat('Y-m-d\TH:i:s\Z', $event->time, new DateTimeZone('UTC'))
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -2,20 +2,20 @@
 <nav class="navbar navbar-expand-lg bg-primary fixed-top">
     <div class="container">
         <div class="navbar-translate">
-            <a class="navbar-brand" href="https://sccodes.org/" rel="tooltip" title="LEARN PROGRAMMING. BUILD THE [FUTURE];" data-placement="bottom" target="_blank">
+            <a class="navbar-brand" href="https://sccodes.org/" data-toggle="tooltip" title="LEARN PROGRAMMING. BUILD THE [FUTURE];" data-placement="bottom" target="_blank">
                 <i class="fa fa-graduation-cap"></i>
                 <span>SC Codes</span>
             </a>
             
             <!-- I think this makes a menu bar on small screens. Not sure. -->
-            <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-bar bar1"></span>
-                <span class="navbar-toggler-bar bar2"></span>
-                <span class="navbar-toggler-bar bar3"></span>
-            </button>
+            <!--<button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">-->
+            <!--    <span class="navbar-toggler-bar bar1"></span>-->
+            <!--    <span class="navbar-toggler-bar bar2"></span>-->
+            <!--    <span class="navbar-toggler-bar bar3"></span>-->
+            <!--</button>-->
         </div>
         
-        <div class="collapse navbar-collapse justify-content-end" data-nav-image="../resources/assets/img/blurred-image-1.jpg">
+        <div class="collapse navbar-collapse justify-content-end" data-nav-image="/img/blurred-image-1.jpg">
             <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link" href="/calendar">
@@ -37,13 +37,13 @@
                 </li>
                     
                 <li class="nav-item">
-                    <a class="nav-link" rel="tooltip" title="Check out the Hack Greenville Slack" data-placement="bottom" href="https://hackgreenville.typeform.com/to/sBMjCF" target="_blank">
+                    <a class="nav-link" data-toggle="tooltip" title="Check out the Hack Greenville Slack" data-placement="bottom" href="https://hackgreenville.typeform.com/to/sBMjCF" target="_blank">
                         <i class="fa fa-slack"></i>
                         <p class="d-lg-none d-xl-none">Slack</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" rel="tooltip" title="Follow SC Codes on Twitter" data-placement="bottom" href="https://twitter.com/sc_codes" target="_blank">
+                    <a class="nav-link" data-toggle="tooltip" title="Follow SC Codes on Twitter" data-placement="bottom" href="https://twitter.com/sc_codes" target="_blank">
                         <i class="fa fa-twitter"></i>
                         <p class="d-lg-none d-xl-none">Twitter</p>
                     </a>
@@ -52,14 +52,14 @@
                 <!-- Facebook boilerplate -->
                 
                 <!--<li class="nav-item">-->
-                <!--    <a class="nav-link" rel="tooltip" title="Like us on Facebook" data-placement="bottom" href="#" target="_blank">-->
+                <!--    <a class="nav-link" title="Like us on Facebook" data-placement="bottom" href="#" target="_blank">-->
                 <!--        <i class="fa fa-facebook-square"></i>-->
                 <!--        <p class="d-lg-none d-xl-none">Facebook</p>-->
                 <!--    </a>-->
                 <!--</li>-->
                 
                 <li class="nav-item">
-                    <a class="nav-link" rel="tooltip" title="Follow SC Codes on Instagram" data-placement="bottom" href="https://www.instagram.com/sc_codes/" target="_blank">
+                    <a class="nav-link" data-toggle="tooltip" title="Follow SC Codes on Instagram" data-placement="bottom" href="https://www.instagram.com/sc_codes/" target="_blank">
                         <i class="fa fa-instagram"></i>
                         <p class="d-lg-none d-xl-none">Instagram</p>
                     </a>
