677b7aab65075f10f854220043a7a235273eb5f9
Creighton Magoun <magoun@gmail.com>
2018-10-23 15:33:58 -0500

    Clean up Now UI js and css calls

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -9,35 +9,22 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
 
 <!-- CSS Files -->
-<link href="css/bootstrap.min.css" rel="stylesheet" />
-<link href="css/now-ui-kit.css?v=1.1.0" rel="stylesheet" />
-<link href="css/style.css" rel="stylesheet" />
+<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" />
+
+<link href="{{ asset('css/style.css') }}" rel="stylesheet" />
 
 <!--   Core JS Files   -->
-<script src="js/core/jquery.3.2.1.min.js"></script>
-<script src="js/core/popper.min.js"></script>
-<script src="js/core/bootstrap.min.js"></script>
+<script src="{{ asset('js/core/jquery.3.2.1.min.js') }}"></script>
+<script src="{{ asset('js/core/popper.min.js') }}"></script>
+<script src="{{ asset('js/core/bootstrap.min.js') }}"></script>
 
 <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
-<script src="js/plugins/bootstrap-switch.js"></script>
-
-<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-<script src="js/plugins/nouislider.min.js"></script>
+<script src="{{ asset('js/plugins/bootstrap-switch.js') }}"></script>
 
 <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
-<script src="js/plugins/bootstrap-datepicker.js"></script>
+<script src="{{ asset('js/plugins/bootstrap-datepicker.js') }}"></script>
 
 <!-- Share Library etc -->
-<script src="js/plugins/jquery.sharrre.js"></script>
-
-<!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
-<script src="js/now-ui-kit.js?v=1.1.0"></script>
-
-<script>
-    $(document).ready(function() {
-        // the body of this function is in assets/js/now-ui-kit.js
-        // nowuiKit.initSliders();
-    });
-</script>
+<script src="{{ asset('js/plugins/jquery.sharrre.js') }}"></script>
 
 <title>@yield('title')</title>
