f09f2e6778d64e87763931cc18f05ead0c2a7a89
Creighton Magoun <magoun@gmail.com>
2018-10-23 20:57:38 -0500

    Rewrite navigation bar without Now UI kit and remove unnecessary CSS

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,75 +1,73 @@
 <!-- Navbar -->
-<nav class="navbar navbar-dark navbar-expand-lg bg-primary fixed-top">
-    <!--<div class="container">-->
-        <!--<div class="navbar-translate">-->
-            <a class="navbar-brand" href="/" data-placement="bottom">
-                <i class="fa fa-server"></i>
-                <span>HackGreenville</span>
-            </a>
-            
-             <!-- This creates and controls a dropdown menu on small screens -->
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        </div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+  <a class="navbar-brand" href="/">
+    <i class="fa fa-server"></i>
+    <span>HackGreenville</span>
+  </a>
+  
+  <!--This creates and controls a dropdown menu on small screens -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  
+  <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="/calendar">
+          <i class="fa fa-calendar"></i>
+          <span>Calendar</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="/events">
+          <i class="fa fa-calendar-check-o"></i>
+          <span>Events</span>
+        </a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="/orgs">
+          <i class="fa fa-users"></i>
+          <span>Organizations</span>
+        </a>
+      </li>
         
-        <div class="collapse navbar-collapse justify-content-end" id="navigation">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="/calendar">
-                        <i class="fa fa-calendar"></i>
-                        <p>Calendar</p>
-                    </a>
-                </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="tooltip" title="Check out the Hack Greenville Slack" data-placement="bottom" href="https://hackgreenville.slack.com" target="_blank">
+          <i class="fa fa-slack"></i>
+          <span>Slack</span>
+        </a>
+      </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="/events">
-                        <i class="fa fa-calendar-check-o"></i>
-                        <p>Events</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/orgs">
-                        <i class="fa fa-users"></i>
-                        <p>Organizations</p>
-                    </a>
-                </li>
-                    
-                <li class="nav-item">
-                    <a class="nav-link" data-toggle="tooltip" title="Check out the Hack Greenville Slack" data-placement="bottom" href="https://hackgreenville.slack.com" target="_blank">
-                        <i class="fa fa-slack"></i>
-                        <p class="d-lg-none d-xl-none">Slack</p>
-                    </a>
-                </li>
+      <!--  Twitter boilerplate -->
 
-                <!--  Twitter boilerplate -->
+      <!--<li class="nav-item">-->
+      <!--    <a class="nav-link" data-toggle="tooltip" title="Follow SC Codes on Twitter" data-placement="bottom" href="https://twitter.com/sc_codes" target="_blank">-->
+      <!--        <i class="fa fa-twitter"></i>-->
+      <!--        <p class="d-lg-none d-xl-none">Twitter</p>-->
+      <!--    </a>-->
+      <!--</li>-->
+      
+      <!-- Facebook boilerplate -->
+      
+      <!--<li class="nav-item">-->
+      <!--    <a class="nav-link" title="Like us on Facebook" data-placement="bottom" href="#" target="_blank">-->
+      <!--        <i class="fa fa-facebook-square"></i>-->
+      <!--        <p class="d-lg-none d-xl-none">Facebook</p>-->
+      <!--    </a>-->
+      <!--</li>-->
 
-                <!--<li class="nav-item">-->
-                <!--    <a class="nav-link" data-toggle="tooltip" title="Follow SC Codes on Twitter" data-placement="bottom" href="https://twitter.com/sc_codes" target="_blank">-->
-                <!--        <i class="fa fa-twitter"></i>-->
-                <!--        <p class="d-lg-none d-xl-none">Twitter</p>-->
-                <!--    </a>-->
-                <!--</li>-->
-                
-                <!-- Facebook boilerplate -->
-                
-                <!--<li class="nav-item">-->
-                <!--    <a class="nav-link" title="Like us on Facebook" data-placement="bottom" href="#" target="_blank">-->
-                <!--        <i class="fa fa-facebook-square"></i>-->
-                <!--        <p class="d-lg-none d-xl-none">Facebook</p>-->
-                <!--    </a>-->
-                <!--</li>-->
-
-                <!-- Instagram boilerplate -->
-                
-                <!--<li class="nav-item">-->
-                <!--    <a class="nav-link" data-toggle="tooltip" title="Follow SC Codes on Instagram" data-placement="bottom" href="https://www.instagram.com/sc_codes/" target="_blank">-->
-                <!--        <i class="fa fa-instagram"></i>-->
-                <!--        <p class="d-lg-none d-xl-none">Instagram</p>-->
-                <!--    </a>-->
-                <!--</li>-->
-            </ul>
-        <!--</div>-->
-    <!--</div>-->
+      <!-- Instagram boilerplate -->
+      
+      <!--<li class="nav-item">-->
+      <!--    <a class="nav-link" data-toggle="tooltip" title="Follow SC Codes on Instagram" data-placement="bottom" href="https://www.instagram.com/sc_codes/" target="_blank">-->
+      <!--        <i class="fa fa-instagram"></i>-->
+      <!--        <p class="d-lg-none d-xl-none">Instagram</p>-->
+      <!--    </a>-->
+      <!--</li>-->
+    </ul>
+  </div>
 </nav>
+
 <!-- End Navbar -->
