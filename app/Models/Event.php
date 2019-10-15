b18ba6da7d86eeb6f434335e934649a52c33553a
Zach2825 <zach2825@gmail.com>
2019-10-15 14:35:24 -0400

    started to add work for event tracking

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,10 +2,46 @@
 
 namespace App\Models;
 
+use DB;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Event extends Model
 {
     use SoftDeletes;
+
+    protected $table = '';
+
+    protected $fillable
+        = [
+            'slug',
+            'name',
+            'description',
+            'venue_id',
+            'cache',
+            'scheduled_at',
+        ];
+
+    protected $casts
+        = [
+            'cache' => 'json',
+        ];
+
+    protected $dates
+        = [
+            'created_at',
+            'updated_at',
+            'deleted_at',
+            'scheduled_at',
+        ];
+
+    public function venue()
+    {
+        return $this->belongsTo(Venue::class);
+    }
+
+    public function scopeGetActive($query)
+    {
+        return $query->where('scheduled_at', '>=', DB::raw('UTC_TIME'));
+    }
 }
---
+++ database/migrations/2019_10_15_141741_create_events_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('slug');
+            $table->string('name');
+            $table->longText('description');
+
+            $table->unsignedBigInteger('venue_id');
+
+            $table->longText('cache');
+
+            $table->dateTime('scheduled_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
