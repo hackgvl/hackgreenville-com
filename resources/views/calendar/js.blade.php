8c59d738a8ae96ec82848d717085256570bc514d
Zach2825 <zach2825@gmail.com>
2019-11-06 13:38:51 -0500

    Calendar modifications to make the UI better

--- resources/views/calendar/js.blade.php
+++ resources/views/calendar/js.blade.php
@@ -6,24 +6,43 @@
             plugins: ['dayGrid'],
             eventSources: [{events: {!! $events !!} }],
             eventClick: function (info) {
-                let desc = info.event.extendedProps.description.replace(/\<a/, '<a target="_blank"');
+                const event_link = info.event.extendedProps.event_url;
+                const datetime_format = 'MM/DD hh:mm A';
+
+                let calendar_desc = `Event link: ${event_link}`;
+                calendar_desc += "<br />";
+                calendar_desc += info.event.extendedProps.description;
+
+                let desc = `<div class="text-left">`;
+                desc += `<strong>Dates:</strong> ${moment(info.event.start).format(`(dddd) ${datetime_format}`)} - ${moment(info.event.end).format(datetime_format)}`;
+                desc += "<br />";
+                desc += "<br />";
+                desc += info.event.extendedProps.description.replace(/\<a/, '<a target="_blank"');
+                desc += "</div>";
+
                 swal.fire({
                     title: info.event.title,
                     html: desc,
                     type: 'info',
                     showCancelButton: true,
                     confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: 'Add it to my calendar!'
+                    cancelButtonColor: '#00a508',
+                    confirmButtonText: 'Add to Google Calendar!',
+                    cancelButtonText: 'Visit event page',
+                    showCloseButton: true,
                 }).then((result) => {
                     if (result.value) {
-                        const link = "https://calendar.google.com/calendar/r/eventedit?" +
-                            `text=${info.event.title}&` +
-                            `dates=${info.event.extendedProps.start_fmt}/${info.event.extendedProps.end_fmt}&` +
-                            `details=${desc}&` +
-                            `location=${info.event.extendedProps.location}`;
+
+                        let link = 'https://calendar.google.com/calendar/r/eventedit?text=${info.event.title}&';
+                        link += `dates=${info.event.extendedProps.start_fmt}/${info.event.extendedProps.end_fmt}&`;
+                        link += `details=${calendar_desc}&`;
+                        link += `location=${info.event.extendedProps.location}`;
+
+                        link = encodeURI(link);
 
                         window.open(link, '_blank');
+                    } else if (result.dismiss == "cancel") {
+                        window.open(event_link, '_blank');
                     }
                 });
 
