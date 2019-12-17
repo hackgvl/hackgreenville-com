118aaace9db02e894f977dd262413b8c079c1b2b
Zach2825 <zach2825@gmail.com>
2019-12-17 10:44:53 -0500

    removed the link for @webdood and removed nofollow and _blank link attributes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,22 +3,21 @@
 @section('content')
     <div class="container">
         <h1>About HackGreenville</h1>
-        <p>HackGreenville was started as a <a target="_blank" href="https://hackgreenville.com/join-slack" rel="nofollow">Slack chat group</a> in March 2015 by Andrew Orr
-            (<a target="_blank" class="user-mention" data-hovercard-type="user" data-hovercard-url="/users/webdood/hovercard" data-octo-click="hovercard-link-click" data-octo-dimensions="link_type:self"
-                href="https://github.com/webdood">@webdood</a>)
-            and Dave Brothers (<a target="_blank" class="user-mention" data-hovercard-type="user" data-hovercard-url="/users/davebrothers/hovercard" data-octo-click="hovercard-link-click"
+        <p>HackGreenville was started as a <a href="https://hackgreenville.com/join-slack">Slack chat group</a> in March 2015 by Andrew Orr
+            (@webdood)
+            and Dave Brothers (<a class="user-mention" data-hovercard-type="user" data-hovercard-url="/users/davebrothers/hovercard" data-octo-click="hovercard-link-click"
                                   data-octo-dimensions="link_type:self" href="https://github.com/davebrothers">@davebrothers</a>).</p>
-        <p>The <a target="_blank" href="https://hackgreenville.com/join-slack" rel="nofollow">Slack community</a> has stood the "test of time" and continues to be a daily, active hub for countless and varied
+        <p>The <a href="https://hackgreenville.com/join-slack">Slack community</a> has stood the "test of time" and continues to be a daily, active hub for countless and varied
             conversations.</p>
-        <p>The HackGreenville.com website was initially developed by the <a target="_blank" href="https://www.sccodes.org" rel="nofollow">SC Codes</a>
-            pilot program and later adopted and expanded by <a target="_blank" href="https://codeforgreenville.org" rel="nofollow">Code
-                For Greenville</a>. The site leverages <a target="_blank" href="https://data.openupstate.org" rel="nofollow">open data APIs developed by Code For Greenville</a> to promote local, related meetup groups and
+        <p>The HackGreenville.com website was initially developed by the <a href="https://www.sccodes.org">SC Codes</a>
+            pilot program and later adopted and expanded by <a href="https://codeforgreenville.org">Code
+                For Greenville</a>. The site leverages <a href="https://data.openupstate.org">open data APIs developed by Code For Greenville</a> to promote local, related meetup groups and
             events.</p>
         <p>The following summarizes the overall community's purpose, mission, vision, and culture.</p>
         <h2>Purpose</h2>
         <p>HackGreenville exists to foster personal growth among the "hackers" of Greenville, SC and the surrounding area.</p>
         <h2>Mission</h2>
-        <p>HackGreenville hosts an <a target="_blank" href="https://hackgreenville.com/join-slack" rel="nofollow">online community</a> and <a target="_blank" href="https://hackgreenville.com" rel="nofollow">discovery
+        <p>HackGreenville hosts an <a href="https://hackgreenville.com/join-slack">online community</a> and <a href="https://hackgreenville.com">discovery
                 tools</a> to promote local, non-commercial tech opportunities for learning, sharing, and connecting.</p>
         <h2>Vision</h2>
         <p>HackGreenville desires to become the <code>GO TO</code> (pun intended!) brand and resource for tech people discovering the area's "hacker" culture and opportunities.</p>
