7dd29bd4da9a1a543a2adb95e3deaac274bfbadf
Zach Robichaud <zach2825@gmail.com>
2020-09-09 14:56:03 -0400

    HG-63 - cleanup canceled events
    resolves #63
    
    Added command to fill event_uuids and let the api update the events better and import events missing locations.

--- app/Console/Commands/PullEventsCommand.php
+++ app/Console/Commands/PullEventsCommand.php
@@ -6,6 +6,7 @@ use App\Http\Clients\UpstateClient;
 use App\Models\Event;
 use App\Models\State;
 use App\Models\Venue;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Collection;
 
@@ -16,7 +17,9 @@ class PullEventsCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'pull:events {--1|one : just import one event} {--d|debug : dump the first response from the events api}';
+    protected $signature = 'pull:events ' .
+                           '{--1|one : just import one event} ' .
+                           '{--d|debug : dump the first response from the events api}';
 
     /**
      * The console command description.
@@ -40,21 +43,6 @@ class PullEventsCommand extends Command
 
         $bar = $this->output->createProgressBar(count($events));
         $bar->start();
-        $events_missing_venue = [];
-
-        $current_active_events = Event::getActive()->get();
-
-        // Loop the current events, if they don't exist in the api response events then delete them.
-	    foreach($current_active_events as $event){
-	    	// TODO :: finish checking to see if the api response contains an active auction. If it does not then remove it from the database
-	    	$found_count = $events
-			    ->where('uuid', $event['uuid'])
-			    ->get('*');
-
-	    	print "Found count: " . $found_count . "\r\n";
-	    }
-
-	    dd('done');
 
         foreach ($events as $inc => $event) {
             $bar->advance();
@@ -63,56 +51,47 @@ class PullEventsCommand extends Command
                 continue;
             }
 
-            if (!$event['venue']) {
-                $events_missing_venue[] = $event;
-                continue;
+            // Start to format the event data. This array can be appended to in the venue conditional statement below
+            $event_data = [
+                'event_uuid'   => $event[ 'uuid' ],
+                'event_name'   => $event[ 'event_name' ],
+                'group_name'   => $event[ 'group_name' ],
+                'description'  => $event[ 'description' ],
+                'rsvp_count'   => $event[ 'rsvp_count' ],
+                'active_at'    => $event[ 'localtime' ],
+                'is_cancelled' => $event[ 'status' ] == 'canceled' ? new Carbon() : null,
+                'uri'          => $event[ 'url' ] ?: 'https://www.meetup.com/Hack-Greenville/events/',
+                'cache'        => $event,
+            ];
+
+            if (array_get($event, 'venue')) {
+                // make sure to get a real state
+                $event_state = array_get($event, 'venue.state') ?: 'SC';
+                $state       = State::where('abbr', 'like', $event_state)->first();
+
+                // make sure the venue exists in the system
+                $venue = Venue::firstOrCreate([
+                    'address'  => array_get($event, 'venue.address'),
+                    'zipcode'  => array_get($event, 'venue.zip'),
+                    'state_id' => $state->id,
+                ], [
+                    'address'  => array_get($event, 'venue.address'),
+                    'zipcode'  => array_get($event, 'venue.zip'),
+                    'state_id' => $state->id,
+                    'city'     => array_get($event, 'venue.city'),
+                    'name'     => array_get($event, 'venue.name'),
+                    'lat'      => array_get($event, 'venue.lat'),
+                    'lng'      => array_get($event, 'venue.lon'),
+                ]);
+
+                $event_data += ['venue_id' => $venue->id];
             }
 
-            // make sure to get a real state
-            $event_state = array_get($event, 'venue.state') ?: 'SC';
-            $state = State::where('abbr', 'like', $event_state)->first();
-
-	        // make sure the venue exists in the system
-            $venue = Venue::firstOrCreate([
-                'address'  => array_get($event, 'venue.address'),
-                'zipcode'  => array_get($event, 'venue.zip'),
-                'state_id' => $state->id,
-            ], [
-                'address'  => array_get($event, 'venue.address'),
-                'zipcode'  => array_get($event, 'venue.zip'),
-                'state_id' => $state->id,
-                'city'     => array_get($event, 'venue.city'),
-                'name'     => array_get($event, 'venue.name'),
-                'lat'      => array_get($event, 'venue.lat'),
-                'lng'      => array_get($event, 'venue.lon'),
-            ]);
-
-            // insert or update the event
-            $test2 = $venue->events()->updateOrCreate([
-                'event_name'  => $event['event_name'],
-                'group_name'  => $event['group_name'],
-                'cache->uuid' => $event['uuid'],
-            ], [
-                'event_name'  => $event['event_name'],
-                'group_name'  => $event['group_name'],
-                'description' => $event['description'],
-                'rsvp_count'  => $event['rsvp_count'],
-                'active_at'   => $event['localtime'],
-                'uri'         => $event['url'] ?: 'https://www.meetup.com/Hack-Greenville/events/',
-                'cache'       => $event,
-            ]);
-
-            // TODO :: remove
-            dd([
-            	'venue' => $venue->id,
-	            'event' => $test2->id,
-            ]);
+            Event::updateOrCreate(['event_uuid' => $event[ 'uuid' ]], $event_data);
         }
         $bar->finish();
 
         $this->info('Done importing');
-
-        $this->warn('Did not import ' . count($events_missing_venue) . ' because they are missing venue information.');
 
         return 0;
     }
