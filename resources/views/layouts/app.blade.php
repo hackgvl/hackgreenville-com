f63c7e2ffcfec0c4fefda34833d479bc85d38af2
Jim Ciallella <1777776+allella@users.noreply.github.com>
2022-03-02 12:57:11 -0500

    Remove leading and trailing newlines on the HTML title tag in the app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,9 +10,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>
-        @yield('title', config('app.name'))
-    </title>
+    <title>@yield('title', config('app.name'))</title>
 
     <link rel="shortcut icon" href="{{asset('favicon.png')}}?1"/>
 
