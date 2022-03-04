e8d644f51065f0eaad157c20950a4340846b4170
oliviasculley <olivia@sculley.dev>
2022-03-03 21:27:43 -0500

    use laravelcollective/html

--- resources/views/contact/contact.blade.php
+++ resources/views/contact/contact.blade.php
@@ -3,74 +3,56 @@
 @section('title', 'Hackgreenville - A Developer Community in the Greenville SC Area')
 
 @section('content')
-    <div id="contact">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12 mx-1 mt-5 text-center">
-                    <h1>{{ __('Contact Us') }}</h1>
+<div id="contact">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 mx-1 mt-5 text-center">
+                <h1>{{ __('Contact Us') }}</h1>
 
-                    <p class="summary">
-                        {{ __('Have a question? Fill out the contact form below and we\'ll try to get back to you as soon as possible!') }}
-                    </p>
-                </div>
+                <p class="summary">
+                    {{ __('Have a question? Fill out the contact form below and we\'ll try to get back to you as soon as possible!') }}
+                </p>
             </div>
+        </div>
 
-            <div class="row">
-                <hr class="mx-auto w-100 px-4" style="max-width: 50em;">
-            </div>
+        <div class="row">
+            <hr class="mx-auto w-100 px-4" style="max-width: 50em;">
+        </div>
 
-            <form method="POST" action="/contact/submit"
-                class="row mx-auto px-5 mb-4 text-center justify-content-center align-items-center"
-                style="max-width: 50em;">
-                @csrf
-
-                <!-- name -->
-                <label for="name" class="col-12 my-1">{{ __('Name') }}</label>
-
-                <input id="name"
-                    type="text"
-                    name="name"
-                    class="text-center col-12 mx-1 my-2 form-control @error('name') is-invalid @enderror"
-                    value="{{ old('name') }}">
-
+        {!! Form::open(['url' => url()->secure('/contact'), 'class' => 'row mx-auto px-5 mb-4 text-center justify-content-center align-items-center', 'style' => 'max-width: 50em;']) !!}
+            <div class="form-group col-12 row">
+                {{ Form::label('name', __('Name'), ['class' => 'col-12 form-label']) }}
+                {{ Form::text('name', old('name'), ['class' => 'col-12 form-control text-center mx-1 my-2' . ($errors->has('name') ? ' is-invalid' : null)]) }}
                 @error('name')
                     <div class="col-12 mx-1 my-2 alert alert-danger justify-content-center align-items-center">{{ $message }}</div>
                 @enderror
+            </div>
 
-                <!-- contact -->
-                <label for="contact" class="col-12 my-1">{{ __('Email') }}</label>
-
-                <input id="contact"
-                    type="email"
-                    name="contact"
-                    class="text-center col-12 mx-1 my-2 form-control @error('contact') is-invalid @enderror"
-                    value="{{ old('contact') }}">
-
+            <div class="form-group col-12 row">
+                {{ Form::label('contact', __('Email'), ['class' => 'col-12 form-label']) }}
+                {{ Form::email('contact', old('contact'), ['class' => 'col-12 form-control text-center mx-1 my-2' . ($errors->has('contact') ? ' is-invalid' : null)]) }}
                 @error('contact')
                     <div class="col-12 mx-1 my-2 alert alert-danger justify-content-center align-items-center">{{ $message }}</div>
                 @enderror
+            </div>
 
-                <!-- message -->
-                <label for="message" class="col-12 my-1">{{ __('Message') }}</label>
-
-                <textarea id="message" name="message"
-                    class="col-12 mx-1 my-2 form-control @error('message') is-invalid @enderror">{{ old('message') }}</textarea>
-
+            <div class="form-group col-12 row">
+                {{ Form::label('message', __('Message'), ['class' => 'col-12 form-label']) }}
+                {{ Form::textarea('message', old('message'), ['class' => 'col-12 form-control text-center mx-1 my-2' . ($errors->has('message') ? ' is-invalid' : null)]) }}
                 @error('message')
                     <div class="col-12 mx-1 my-2 alert alert-danger justify-content-center align-items-center">{{ $message }}</div>
                 @enderror
+            </div>
 
-                {!! HCaptcha::display(['class' => 'hcaptcha col-12 mt-4']) !!}
-                <script src="https://js.hcaptcha.com/1/api.js" async defer></script>
+            {!! HCaptcha::display(['class' => 'hcaptcha col-12 mt-4']) !!}
+            <script src="https://js.hcaptcha.com/1/api.js" async defer></script>
 
-                @error('h-captcha-response')
-                    <div class="col-12 mx-1 my-2 alert alert-danger justify-content-center align-items-center">{{ $message }}</div>
-                @enderror
+            @error('h-captcha-response')
+                <div class="col-12 mx-1 my-2 alert alert-danger justify-content-center align-items-center">{{ $message }}</div>
+            @enderror
 
-                <button type="submit" class="my-4 btn btn-outline-secondary">
-                    {{ __('Submit') }}
-                </button>
-            </form>
-        </div>
+            {{ Form::submit('Submit', ['class' => 'my-4 btn btn-outline-secondary']) }}
+        {!! Form::close() !!}
     </div>
+</div>
 @endsection
