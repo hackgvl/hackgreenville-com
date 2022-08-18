4cdda8e2db7cf3f5c9cc526270a543709fd41457
Bogdan Kharchenko <scriptor@gmail.com>
2022-08-18 00:00:26 -0400

    Improved Event List UI

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -6,29 +6,15 @@ use App\Models\Event;
 
 class EventsController extends Controller
 {
-    public function index()
-    {
-        // Collect unique months format Oct 2019 from the events.
-        $events = Event::search()->get();
-
-        $months = $events
-                ->map(
-                        function (Event $event) {
-                            if (!$event->active_at) {
-                                return false;
-                            }
-
-                            return $event->active_at->format('M Y');
-                        }
-                )
-                ->filter(
-                        function ($month) {
-                            // Filter out months
-                            return (bool)$month;
-                        }
-                )
-                ->unique();
-
-        return view('events.index', compact('events', 'months'));
-    }
+	public function index()
+	{
+		$months = Event::search()
+			->latest('active_at')
+			->get()
+			->groupBy(function(Event $event) {
+				return $event->active_at->format('F Y');
+			});
+		
+		return view('events.index', compact('months'));
+	}
 }
--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -4,90 +4,62 @@
 @section('description', 'A list view of upcoming tech events happening in the Greenville, SC area.')
 
 @section('content')
-    <div class="container">
-        <h1>Events</h1>
-        <form method="get">
-            <div class="col-md-8 offset-md-2">
-                <div class="row">
-                    <div class="col-md-10">
-                        <div class="form-group">
-                            <div class="row">
-                                <label for="month" class="px-3 py-1">
-                                    Choose a month to filter
-                                </label>
-                                <select class="form-control col-md-9" name="month" id="month">
-                                    <option value="">Please select</option>
-                                    @foreach( $months as $month )
-                                        <option value="{{$month}}" @if(request('month') == $month) selected="selected" @endif>
-                                            {{$month}}
-                                        </option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="">
-                        <div class="pull-right">
-                            <button class="btn btn-primary btn-round" type="submit">Submit</button>
-                            <a href="{{explode('?', $_SERVER['REQUEST_URI'])[0]}}" class="btn btn-outline-primary btn-round" role="button">
-                                Clear filter
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </form>
-
-        <br>
-
-        <ul>
-            @foreach( $events as $event )
-                <li class="events" data-date="{{$event->active_at->format('M Y')}}">
-                    @if($event->cancelled_at)
-                        <span class="text-danger">[CANCELLED] </span>
-                        <strong>{{ $event->event_name }}</strong></a> hosted by <strong>{{ $event->group_name }}
-                            @else
-                                <a href="{{$event->url}}"><strong>{{ $event->event_name }}</strong></a> hosted by
-                                <strong>{{ $event->group_name }}</strong>
-                            @endif
-                            <p>Time:</strong>{{ $event->active_at }} <br />
-                        @unless($event->cancelled_at)
-                            [<a href="{{$event->url}}">RSVP</a> ||
-                            <a href="{{ $event->gCalUrl }}" target="_blank">Add to Google Calendar</a>]
-                            @endunless
-                            </p>
-
-                </li>
-            @endforeach
-        </ul>
-
-        <ul>
-       	    <li>This data is sourced from <a href="https://data.openupstate.org" target="_blank">a community-curated REST API</a>.</li>
-            <li>To contribute to this project, please connect with <a href="https://codeforgreenville.org" target="_blank">Code For Greenville.</a></li>
-            <li>To suggest an addition or update to the data, please submit a <a href="https://data.openupstate.org/contact/suggestions">suggestion</a>.</li>
-        </ul>
-
-    </div>
+	<div class="container">
+		<h1>Upcoming Events</h1>
+		
+		<div class="mb-4">
+			<form method="get">
+				
+				<select class="form-control col-md-9" name="month" id="month">
+					<option value="">Filter by month</option>
+					@foreach( $months->keys() as $month )
+						<option value="{{$month}}" @if(request('month') == $month) selected="selected" @endif>
+							{{$month}}
+						</option>
+					@endforeach
+				</select>
+			</form>
+		</div>
+		
+		@foreach($months as $month => $events)
+			<div class="events mb-4" data-date="{{ $month }}">
+				<h3 class="font-weight-bolder">
+					{{ $month }}
+				</h3>
+				@foreach($events as $event)
+					@include('events._item', ['event' => $event])
+				@endforeach
+			</div>
+		
+		@endforeach
+		
+		<ul>
+			<li>This data is sourced from <a href="https://data.openupstate.org" target="_blank">a community-curated REST API</a>.</li>
+			<li>To contribute to this project, please connect with <a href="https://codeforgreenville.org" target="_blank">Code For Greenville.</a></li>
+			<li>To suggest an addition or update to the data, please submit a <a href="https://data.openupstate.org/contact/suggestions">suggestion</a>.</li>
+		</ul>
+	
+	</div>
 @endsection
 
 @section('js')
-    <script type="text/javascript">
-        $(function () {
-            /**
-             * on change filter the events. If there is nothing selected show all the events.
-             **/
-            $("#month").change(function () {
-                const val = $(this).children('option:selected').val();
-                const events = $(".events");
-                if (val) {
-                    events.hide();
-                    $(".events[data-date='" + val + "']").show();
-
-                    return true;
-                }
-
-                events.show();
-            })
-        });
-    </script>
+	<script type="text/javascript">
+	$(function () {
+		/**
+		 * on change filter the events. If there is nothing selected show all the events.
+		 **/
+		$("#month").change(function () {
+			const val = $(this).children('option:selected').val();
+			const events = $(".events");
+			if (val) {
+				events.hide();
+				$(".events[data-date='" + val + "']").show();
+				
+				return true;
+			}
+			
+			events.show();
+		})
+	});
+	</script>
 @endsection
