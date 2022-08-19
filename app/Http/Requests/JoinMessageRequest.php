e5ec8e8af00f563224ba1e70926e8007b43a7459
Olivia Sculley <olivia@sculley.dev>
2022-08-18 22:27:06 -0400

    New Join Slack form (#115)
    * add hcaptcha env vars
    
    * add slack contact webhook env var
    
    * require laravel/slack-notification-channel
    
    * update caniuse
    
    * add contact form message
    
    * add contact html routes
    
    * add hcaptcha
    
    * fix typo
    
    * fix contact icon
    
    * move contact page
    
    * use laravelcollective/html
    
    * use ContactMessageRequest
    
    * remove unneeded classes
    
    * remove unnecessary classes
    
    * add meta description tag changes
    
    * add meta tags to submitted form page as well
    
    * fix spacing
    
    * add custom sign up form
    
    * Quick adds to type match a little
    
    Co-authored-by: The Zach <zach2825@gmail.com>

---
+++ app/Http/Requests/JoinMessageRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class JoinMessageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:255',
+            'contact' => 'required|email:rfc,dns',
+            'reason' => 'required|max:5000',
+            'rules' => 'required|accepted',
+            'h-captcha-response' => 'required|HCaptcha'
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'rules.required' => __('Please accept the rules to join HackGreenville.'),
+            'rules.accepted' => __('You must accept the rules to join HackGreenville.'),
+            'h-captcha-response.required' => __('Please verify that you are not a robot.'),
+            'h-captcha-response.captcha' => __('Captcha error! try again later or contact site admin.'),
+        ];
+    }
+}
