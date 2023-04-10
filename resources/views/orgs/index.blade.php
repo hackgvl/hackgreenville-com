b5064e08b8eb4938f505893b100f2d93d85c4d30
Bogdan Kharchenko <scriptor@gmail.com>
2023-04-10 15:15:58 -0400

    Issue 127/ Organization Page Layout Tweaks

--- resources/views/orgs/index.blade.php
+++ resources/views/orgs/index.blade.php
@@ -7,28 +7,38 @@
     <div class="container">
 
         <h1>Local Tech Organizations</h1>
-
-        <div class="row">
-            @foreach ($activeOrgs as $cat_inc => $category)
-                <div class="col-md-6  mb-3">
+        <div class="card-columns">
+            @foreach ($activeOrgs as $organizations)
+                <div class="card mb-3 d-inline-block">
                     <ul class="list-group">
                         <li class="list-group-item list-group-item-primary">
                             <h2>
                                 <div class="pull-right">
-                                    {{$category->count()}}
+                                    {{$organizations->count()}}
                                 </div>
-                                {{$category[0]->category->label}}
+                                {{$organizations->first()->category->label}}
                             </h2>
                         </li>
 
-                        @foreach($category as $org)
+                        @foreach($organizations as $org)
                             <li class="list-group-item">
                                 <div class="row">
                                     <div class="col-md-6">
-                                        <a href="{{ $org->url}}" rel="external" title="Homepage">{{ $org->title }}</a>
+                                        <a href="{{ $org->url}}" rel="external" title="Homepage"
+                                            @class([
+                                                'text-muted text-decoration-line-through' => $org->category->isInactive()
+                                            ])
+                                        >
+                                            {{ $org->title }}
+                                        </a>
                                     </div>
-                                    <div class="col-md-6">
-                                        <a href="{{$org->event_calendar_uri }}" rel="external">Events Site</a>
+                                    <div class="col-md-6 text-right">
+                                        @if($org->event_calendar_uri)
+                                            <a href="{{$org->event_calendar_uri }}" rel="external">
+                                                Events Site
+                                                <i class="fa fa-external-link"></i>
+                                            </a>
+                                        @endif
                                     </div>
                                 </div>
                             </li>
@@ -48,9 +58,10 @@
             <li>To contribute to this project, please connect with <a href="https://codeforgreenville.org"
                                                                       rel="external">Code For Greenville.</a></li>
             <li>To suggest an addition or update to the data, please submit a <a
-                        href="https://data.openupstate.org/contact/suggestions">suggestion</a>.
+                    href="https://data.openupstate.org/contact/suggestions">suggestion</a>.
             </li>
         </ul>
 
     </div>
+
 @endsection
