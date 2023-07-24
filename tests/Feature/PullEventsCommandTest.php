7c7b69d7610381c91dfd3aee2e39140e3c3c3644
Bogdan Kharchenko <scriptor@gmail.com>
2023-07-24 14:48:25 -0400

    Update tests

--- tests/Feature/PullEventsCommandTest.php
+++ tests/Feature/PullEventsCommandTest.php
@@ -7,64 +7,58 @@ use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Http;
-use Tests\TestCase;
 
-class PullEventsCommandTest extends TestCase
-{
-    use RefreshDatabase;
+uses(RefreshDatabase::class);
 
-    public function test_it_imports_events_from_api(): void
-    {
-        Http::fake([
-            config('app.events_api_domain') . '/*' => Http::response([
-                [
-                    'event_name' => 'Weekly Friday Afternoon Garden Tending',
-                    'group_name' => 'SynergyMill Community Workshop',
-                    'group_url' => 'https://synergymill.com',
-                    'venue' => [
-                        'name' => 'Synergy Mill',
-                        'address' => '400 Birnie St Ext',
-                        'city' => 'Greenville',
-                        'state' => 'SC',
-                        'zip' => '',
-                        'country' => 'us',
-                        'lat' => 34.849491119385,
-                        'lon' => -82.414642333984,
-                    ],
-                    'url' => 'https://www.meetup.com/synergymill/events/kqzwctyfcjbnc/',
-                    'time' => '2023-06-30T20:00:00Z',
-                    'tags' => 1,
-                    'rsvp_count' => 1,
-                    'created_at' => '2023-03-22T15:08:22Z',
-                    'description' => 'We\'ll be tending the garden...',
-                    'uuid' => 'cad24922-f53c-42ee-a7d0-eb15f74b7d86',
-                    'nid' => 25,
-                    'data_as_of' => '2023-07-01T14:40:08Z',
-                    'status' => 'past',
-                    'service_id' => 'kqzwctyfcjbnc',
-                    'service' => 'meetup',
-                    'localtime' => Carbon::parse('2023-06-30 16:00:00'),
+it("it imports events from api", function () {
+    Http::fake([
+        config('app.events_api_domain') . '/*' => Http::response([
+            [
+                'event_name' => 'Weekly Friday Afternoon Garden Tending',
+                'group_name' => 'SynergyMill Community Workshop',
+                'group_url' => 'https://synergymill.com',
+                'venue' => [
+                    'name' => 'Synergy Mill',
+                    'address' => '400 Birnie St Ext',
+                    'city' => 'Greenville',
+                    'state' => 'SC',
+                    'zip' => '',
+                    'country' => 'us',
+                    'lat' => 34.849491119385,
+                    'lon' => -82.414642333984,
                 ],
-            ], 200),
-        ]);
+                'url' => 'https://www.meetup.com/synergymill/events/kqzwctyfcjbnc/',
+                'time' => '2023-06-30T20:00:00Z',
+                'tags' => 1,
+                'rsvp_count' => 1,
+                'created_at' => '2023-03-22T15:08:22Z',
+                'description' => 'We\'ll be tending the garden...',
+                'uuid' => 'cad24922-f53c-42ee-a7d0-eb15f74b7d86',
+                'nid' => 25,
+                'data_as_of' => '2023-07-01T14:40:08Z',
+                'status' => 'past',
+                'service_id' => 'kqzwctyfcjbnc',
+                'service' => 'meetup',
+                'localtime' => Carbon::parse('2023-06-30 16:00:00'),
+            ],
+        ], 200),
+    ]);
 
-        Artisan::call(PullEventsCommand::class);
+    Artisan::call(PullEventsCommand::class);
 
-        $this->assertDatabaseHas('events', [
-            'event_name' => 'Weekly Friday Afternoon Garden Tending',
-            'group_name' => 'SynergyMill Community Workshop',
-            'uri' => 'https://www.meetup.com/synergymill/events/kqzwctyfcjbnc/'
-        ]);
+    $this->assertDatabaseHas('events', [
+        'event_name' => 'Weekly Friday Afternoon Garden Tending',
+        'group_name' => 'SynergyMill Community Workshop',
+        'uri' => 'https://www.meetup.com/synergymill/events/kqzwctyfcjbnc/'
+    ]);
 
-        $this->assertDatabaseHas('venues', [
-            'name' => 'Synergy Mill',
-            'address' => '400 Birnie St Ext',
-            'city' => 'Greenville',
-            'lat' => 34.849491119385,
-            'lng' => -82.414642333984,
-        ]);
+    $this->assertDatabaseHas('venues', [
+        'name' => 'Synergy Mill',
+        'address' => '400 Birnie St Ext',
+        'city' => 'Greenville',
+        'lat' => 34.849491119385,
+        'lng' => -82.414642333984,
+    ]);
 
-        Http::assertSent(fn ($request) => $request->url() === config('app.events_api_domain') . '/api/gtc');
-    }
-
-}
+    Http::assertSent(fn($request) => $request->url() === config('app.events_api_domain') . '/api/gtc');
+});
