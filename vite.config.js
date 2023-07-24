43f99f84147b3a13d6067ff6b5071afcda6612b7
Bogdan Kharchenko <scriptor@gmail.com>
2023-07-24 16:42:59 -0400

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
+import {ViteImageOptimizer} from "vite-plugin-image-optimizer";
 
 export default defineConfig({
     plugins: [
@@ -10,6 +11,35 @@ export default defineConfig({
                 'resources/js/app.js',
             ],
             refresh: true,
+        }),
+        ViteImageOptimizer({
+            test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+            includePublic: true,
+            ansiColors: true,
+            png: {
+                // https://sharp.pixelplumbing.com/api-output#png
+                quality: 85,
+            },
+            jpeg: {
+                // https://sharp.pixelplumbing.com/api-output#jpeg
+                quality: 80,
+            },
+            jpg: {
+                // https://sharp.pixelplumbing.com/api-output#jpeg
+                quality: 80,
+            },
+            tiff: {
+                // https://sharp.pixelplumbing.com/api-output#tiff
+                quality: 100,
+            },
+            webp: {
+                // https://sharp.pixelplumbing.com/api-output#webp
+                lossless: true,
+            },
+            avif: {
+                // https://sharp.pixelplumbing.com/api-output#avif
+                lossless: true,
+            },
         }),
     ],
     server: {
