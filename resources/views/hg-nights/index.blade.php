8742b4a148465e16b63b016d4505c6b0e43c6d82
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2023-11-07 23:12:16 -0500

    Add stragglers

---
+++ resources/views/hg-nights/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'HackGreenville Nights')
+@section('description', 'An evening where Greenville\'s tech community gathers for fellowship and short-format talks')
+
+@section('content')
+    <div id="about-us" class="container">
+        <h1 class="title-heading">HackGreenville Nights</h1>
+        <p>I am an underwhelming placeholder.</p>
+    </div>
+@endsection
