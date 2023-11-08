a28fe18172ed911e5c8f47567dabdf1086a3c6e8
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2023-11-07 23:36:37 -0500

    Fix div id

--- resources/views/hg-nights/index.blade.php
+++ resources/views/hg-nights/index.blade.php
@@ -4,7 +4,7 @@
 @section('description', 'An evening where Greenville\'s tech community gathers for fellowship and short-format talks')
 
 @section('content')
-    <div id="about-us" class="container">
+    <div id="hg-nights-info" class="container">
         <h1 class="title-heading">HackGreenville Nights</h1>
         <p>I am an underwhelming placeholder.</p>
     </div>
