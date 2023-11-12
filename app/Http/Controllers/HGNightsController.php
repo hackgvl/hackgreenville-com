60d1af95f920be302fd1a2814059f560ff0aaa07
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2023-11-12 00:01:22 -0500

    Add some comments

--- app/Http/Controllers/HGNightsController.php
+++ app/Http/Controllers/HGNightsController.php
@@ -18,6 +18,12 @@ class HGNightsController extends Controller
         return view('hg-nights.index')->with('events', self::events_data());
     }
 
+    /**
+     * Parses HG Nights events data from all JSON files in resources/data/hg-nights
+     * and places the parsed JSON arrays into an array.
+     *
+     * @return array<array<mixed>
+     */
     protected function events_data()
     {
         $event_files = self::gather_event_files();
@@ -25,6 +31,11 @@ class HGNightsController extends Controller
         return array_map([$this, 'parse_event'], $event_files);
     }
 
+    /**
+     * Finds all files in resources/data/hg-nights that are JSON files.
+     *
+     * @return array<string>
+     */
     private function gather_event_files()
     {
         return preg_grep(
@@ -33,6 +44,11 @@ class HGNightsController extends Controller
         );
     }
 
+    /**
+     * Parses the string contents of a JSON file into an array.
+     *
+     * @return array<mixed>
+     */
     private function parse_event($event_file)
     {
         $jsonString = file_get_contents(
