18f3d2b8de6649da1313b780d6b9028e64d33f8d
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2023-11-21 22:33:53 -0500

    CSS tweaks

--- resources/views/hg-nights/index.blade.php
+++ resources/views/hg-nights/index.blade.php
@@ -5,10 +5,13 @@
 
 @section('content')
   <div class="container">
-    <div id="hg-nights-jumbotron" class="row text-white bg-white jumbotron jumbotron-fluid d-flex flex-column align-items-stretch">
-      <div class="col-sm-12 offset-md-2 col-md-4 py-5  justify-content-center text-center">
-        <h1 class="display-3">{{ __('HackGreenville Nights') }}</h1>
-        <div class="p-2 text-black bg-white rounded">
+    <div
+      id="hg-nights-jumbotron"
+      class="row text-white jumbotron jumbotron-fluid d-flex flex-column align-items-stretch"
+    >
+      <div class="col-sm-10 offset-md-2 col-md-4 py-5  justify-content-center text-center">
+        <h1 class="display-1">{{ __('HackGreenville Nights') }}</h1>
+        <div class="p-2 text-primary bg-light rounded col-sm-12">
           <p class="lead">
             A “Quarterly-ish” Gathering of Greenville's Tech Community Coming
             Together to Learn From Each Other and to Have Fellowship With One Another.
@@ -34,30 +37,10 @@
         <li class="list-group-item">Help make HG Nights possible by <a href="/contact">becoming a sponsor</a></li>
       </ul>
     </div>
-    <div class="col-10 px-5 offset-2 col-sm-8 offset-sm-4 col-xl-12 offset-xl-2 mt-5 mx-auto d-flex flex-column">
+    <div
+      class="col-10 px-5 offset-2 col-sm-8 offset-sm-4 col-xl-12 offset-xl-2 mt-5 mx-auto d-flex flex-column"
+    >
       <h2 class="font-weight-bold">{{ __('Past Talks') }}</h2>
-      @foreach($events as $eventIndex => $event)
-        <div class="row col-10">
-          <div class="row col-md-4">
-            <h4 class="font-weight-bold">{{__($event['title'])}}</h4>
-          </div>
-          <div class="row col-md-6">
-            <h4 class="">{{__($event['date'])}} at {{__($event['venue'])}}</h4>
-          </div>
-        </div>
-        <div class="accordion mb-3 p-0" id="accordion{{$eventIndex}}">
-          @foreach($event['talks'] as $talkIndex => $talk)
-            @include('partials.event-card', [
-              'id' => $eventIndex . $talkIndex,
-              'parent_id' => "#accordion" . $eventIndex,
-              'talk' => [
-                'title' => $talk['title'],
-                'speaker' => $talk['speaker']
-              ]])
-          @endforeach
-        </div>
-      @endforeach
-      <div>
     </div>
   </div>
 @endsection
