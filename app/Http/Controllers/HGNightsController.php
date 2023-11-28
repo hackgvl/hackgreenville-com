106ed3bee4d5c69b16a6595d2c6dd25788e0be44
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2023-11-28 13:27:23 -0500

    Remove no longer needed items

--- app/Http/Controllers/HGNightsController.php
+++ app/Http/Controllers/HGNightsController.php
@@ -2,18 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\EventDataService;
 use Illuminate\View\View;
 
 class HGNightsController extends Controller
 {
-    private $eventDataService;
-
-    public function __construct(EventDataService $eventDataService)
-    {
-        $this->eventDataService = $eventDataService;
-    }
-
     /**
      * Present the HackGreenville Nights Page
      *
@@ -21,6 +13,6 @@ class HGNightsController extends Controller
      */
     public function index()
     {
-        return view('hg-nights.index')->with('events', $this->eventDataService->eventsData());
+        return view('hg-nights.index');
     }
 }
--- resources/views/hg-nights/index.blade.php
+++ resources/views/hg-nights/index.blade.php
@@ -27,7 +27,7 @@
         </div>
       </div>
     </div>
-    <div class="col-10 px-5 offset-2 col-sm-8 offset-sm-4 col-xl-12 offset-xl-2 mt-5 mx-auto d-flex flex-column">
+    <div class="col-10 px-5 offset-2 col-sm-8 offset-sm-4 col-xl-12 offset-xl-2 m-5 mx-auto d-flex flex-column">
       <h2 class="font-weight-bold">{{ __('How to Get Involved') }}</h2>
       <ul class="list-group list-group-flush">
         <li class="list-group-item">Join our <a href="https://www.meetup.com/hack-greenville/">Meetup group</a> to receive updates on future events and come hang out!</li>
@@ -36,11 +36,6 @@
         <li class="list-group-item"><a href="/contact">Volunteer</a> to assist with facilitating a gathering</li>
         <li class="list-group-item">Help make HG Nights possible by <a href="/contact">becoming a sponsor</a></li>
       </ul>
-    </div>
-    <div
-      class="col-10 px-5 offset-2 col-sm-8 offset-sm-4 col-xl-12 offset-xl-2 mt-5 mx-auto d-flex flex-column"
-    >
-      <h2 class="font-weight-bold">{{ __('Past Talks') }}</h2>
     </div>
   </div>
 @endsection
