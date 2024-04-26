160c878a9f1a6b4e742a6f1f444d2915d9d05b74
Bogdan Kharchenko <scriptor@gmail.com>
2024-04-26 12:35:41 -0400

    Update 2024_04_26_122917_add_slug_and_description_to_orgs_table.php

--- database/migrations/2024_04_26_122917_add_slug_and_description_to_orgs_table.php
+++ database/migrations/2024_04_26_122917_add_slug_and_description_to_orgs_table.php
@@ -5,22 +5,22 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-	public function up(): void
-	{
-		Schema::table('orgs', function(Blueprint $table) {
-			$table->string('slug')->nullable()->after('id');
-		});
+    public function up(): void
+    {
+        Schema::table('orgs', function (Blueprint $table) {
+            $table->string('slug')->nullable()->after('id');
+        });
 
-		Schema::table('orgs', function(Blueprint $table) {
-			$table->longText('description')->nullable()->after('title');
-		});
-	}
+        Schema::table('orgs', function (Blueprint $table) {
+            $table->longText('description')->nullable()->after('title');
+        });
+    }
 
-	public function down(): void
-	{
-		Schema::table('orgs', function(Blueprint $table) {
-			$table->dropColumn('slug');
-			$table->dropColumn('description');
-		});
-	}
+    public function down(): void
+    {
+        Schema::table('orgs', function (Blueprint $table) {
+            $table->dropColumn('slug');
+            $table->dropColumn('description');
+        });
+    }
 };
