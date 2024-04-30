b0c20b4a66870e2dba873466746c8f2c81084995
Bogdan Kharchenko <scriptor@gmail.com>
2024-04-29 20:57:36 -0400

    Re-Organize the routes, better grouping, new route syntax

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,32 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\CalendarController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\EventsController;
+use App\Http\Controllers\GiveController;
+use App\Http\Controllers\HGNightsController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LabsController;
+use App\Http\Controllers\OrgsController;
+use App\Http\Controllers\SlackController;
+use App\Http\Controllers\StyleController;
 
-Route::get('/calendar', 'CalendarController@index')->name('calendar.index');
-Route::get('/contact', 'ContactController@contact')->name('contact');
-Route::post('/contact', 'ContactController@submit')->name('contact.submit');
-Route::get('/events', 'EventsController@index')->name('events.index');
-Route::get('/labs', 'LabsController@index')->name('labs.index');
-Route::get('/hg-nights', 'HGNightsController@index')->name('hg-nights.index');
-Route::get('/orgs', 'OrgsController@index')->name('orgs.index');
-Route::get('/orgs/inactive', 'OrgsController@inactive')->name('orgs.inactive');
-Route::get('/about', 'HomeController@about')->name('about');
 
-Route::get('/join-slack', 'SlackController@join')->name('join-slack');
-Route::post('/join-slack', 'SlackController@submit')->name('join-slack.submit');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/calendar', [CalendarController::class, 'index'])->name('calendar.index');
+Route::get('/events', [EventsController::class, 'index'])->name('events.index');
+Route::get('/labs', [LabsController::class, 'index'])->name('labs.index');
+Route::get('/hg-nights', [HGNightsController::class, 'index'])->name('hg-nights.index');
+Route::get('/give', [GiveController::class, 'index'])->name('give');
+Route::get('/about', [HomeController::class, 'about'])->name('about');
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/orgs', [OrgsController::class, 'index'])->name('orgs.index');
+Route::get('/orgs/inactive', [OrgsController::class, 'inactive'])->name('orgs.inactive');
 
-Route::get('/give', 'GiveController@index')->name('give');
+Route::get('/contact', [ContactController::class, 'contact'])->name('contact');
+Route::post('/contact', [ContactController::class, 'submit'])->name('contact.submit');
 
-Route::get('/styles', 'StyleController@index')->name('styles.index');
+Route::get('/join-slack', [SlackController::class, 'join'])->name('join-slack');
+Route::post('/join-slack', [SlackController::class, 'submit'])->name('join-slack.submit');
+
+Route::get('/styles', [StyleController::class, 'index'])->name('styles.index');
