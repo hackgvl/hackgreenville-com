6ad67a786f9c785e081c89d45bcf31dbf2b6d10a
Bogdan Kharchenko <scriptor@gmail.com>
2024-06-20 11:45:01 -0400

    Support for Manually Managing Events

---
+++ app/Filament/Resources/VenueResource/Pages/CreateVenue.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\VenueResource\Pages;
+
+use App\Filament\Resources\VenueResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateVenue extends CreateRecord
+{
+    protected static string $resource = VenueResource::class;
+}
