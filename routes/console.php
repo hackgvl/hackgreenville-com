6e858da793dd35d9cb4e9bea075506f12be43f5d
Bogdan Kharchenko <scriptor@gmail.com>
2024-07-31 09:18:17 -0400

    Remove unused packages + files

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-
 /*
 |--------------------------------------------------------------------------
 | Console Routes
@@ -12,18 +10,3 @@ use Illuminate\Foundation\Inspiring;
 | simple approach to interacting with each command's IO methods.
 |
 */
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->describe('Display an inspiring quote');
-
-Artisan::command('events:back-fill-uuid', function () {
-    $updated_count = App\Models\Event::whereNull('event_uuid')->orWhere('event_uuid', '')
-        ->get()
-        ->each(function ($event) {
-            $event->update(['event_uuid' => $event->cache[ 'uuid' ]]);
-            return $event;
-        })->count();
-
-    print sprintf("Updated %s %s.\r\n", $updated_count, str_plural('event', $updated_count));
-})->describe('Looks for events missing its event_uuid and fills it from the event cache');
