8d0585db0152d7bd11226642d378f9d4f12be948
Bogdan Kharchenko <32746389+bogdankharchenko@users.noreply.github.com>
2024-08-06 10:57:34 -0400

    Additional clean up of calendar (#370)
    * Calendar page clean up
    
    * pint

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -21,7 +21,7 @@ class CalendarController extends Controller
         ]);
 
         $events = Event::query()
-            ->with('organization')
+            ->with('organization', 'venue.state')
             ->whereBetween('active_at', [
                 $request->date('start')->startOfDay(),
                 $request->date('end')->endOfDay(),
@@ -30,21 +30,22 @@ class CalendarController extends Controller
             ->map(function (Event $event) {
                 return [
                     'start' => $event->active_at,
-                    'start_fmt' => $event->active_at->copy()->utc()->format('Ymd\THi00\Z'),
                     'end' => $event->expire_at->copy()->addHours(2),
-                    'end_fmt' => $event->expire_at->copy()->addHours(2)->utc()->format('Ymd\THi00\Z'),
 
                     'title' => str($event->organization->title . "\n" . $event->event_name)
                         ->when($event->isCancelled(), fn (Stringable $str) => $str->prepend('[CANCELLED] '))
                         ->toString(),
                     'description' => str($event->description)
                         ->when($event->isCancelled(), fn (Stringable $str) => $str->prepend('<h3 class="text-danger">This event was cancelled</h3><br />')),
+
                     'allDay' => false,
                     'cancelled' => $event->isCancelled(),
                     'color' => match (true) {
                         $event->isCancelled() => 'red',
                         default => null,
                     },
+                    'add_to_google_calendar_url' => $event->toGoogleCalendarUrl(),
+                    'event_url' => $event->uri,
                 ];
             });
 
