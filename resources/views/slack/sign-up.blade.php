36b0275650a9c1f3d750cb9b8970b27dde6eac36
Mitchell Mahoney <mahoneymitch@gmail.com>
2024-09-23 21:54:25 -0400

    fix: typo

--- resources/views/slack/sign-up.blade.php
+++ resources/views/slack/sign-up.blade.php
@@ -34,7 +34,7 @@
                     {{ aire()->input('name', __('Full Name'))->required() }}
                     {{ aire()->email('contact', __('Email'))->required() }}
                     {{ aire()->textArea('reason', __('Share why you are joining HackGreenville and include any relevant local context that helps validate you\'re not a bot or spammer.'))->rows(4)->placeholder(__('What interests you about HackGreenville? What connections do you have to the Upstate of South Carolina?')) }}
-                    {{ aire()->url('url', __('Please provide a LinkedIn profile link, or similar, to help us distinguish that\'s you\'re a real person with a local connection this community.')) }}
+                    {{ aire()->url('url', __('Please provide a LinkedIn profile link, or similar, to help us distinguish that you\'re a real person with a local connection this community.')) }}
 
                 </div>
             </div>
