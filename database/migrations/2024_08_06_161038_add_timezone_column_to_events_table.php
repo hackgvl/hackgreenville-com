eaf798a55cff94be2e4a2eab29126c7255ea8239
Bogdan Kharchenko <scriptor@gmail.com>
2025-05-22 15:50:45 -0400

    WIP

--- database/migrations/2024_08_06_161038_add_timezone_column_to_events_table.php
+++ database/migrations/2024_08_06_161038_add_timezone_column_to_events_table.php
@@ -7,14 +7,14 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::table('events', function(Blueprint $table) {
+        Schema::table('events', function (Blueprint $table) {
             $table->string('timezone')->after('active_at')->default('America/New_York');
         });
     }
 
     public function down(): void
     {
-        Schema::table('events', function(Blueprint $table) {
+        Schema::table('events', function (Blueprint $table) {
             $table->dropColumn('timezone');
         });
     }
--- database/migrations/2025_05_22_195013_drop_unknown_table.php
+++ database/migrations/2025_05_22_195013_drop_unknown_table.php
@@ -1,12 +1,11 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-	public function up(): void
-	{
-		Schema::drop('unknown');
-	}
+    public function up(): void
+    {
+        Schema::drop('unknown');
+    }
 };
