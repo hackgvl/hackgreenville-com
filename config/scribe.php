a828021019daaac1320b9812e23a53c07614ebfe
Matthew H. Irby <matt.irby@outlook.com>
2025-07-09 11:52:19 -0400

    dynamic base URL for laravel scribe
    Signed-off-by: Matthew H. Irby <matt.irby@outlook.com>

--- config/scribe.php
+++ config/scribe.php
@@ -18,7 +18,7 @@ return [
 
     // The base URL displayed in the docs.
     // If you're using `laravel` type, you can set this to a dynamic string, like '{{ config("app.tenant_url") }}' to get a dynamic base URL.
-    'base_url' => config("app.url"),
+    'base_url' => '{{ config("app.url") }}',
 
     'logo' => '/img/logo-v2.png',
 
