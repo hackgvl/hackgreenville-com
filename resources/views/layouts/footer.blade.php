8aa92ae0151f79ef95d1550a2ca3ea471ee342d6
Matthew H. Irby <matt.irby@outlook.com>
2025-07-11 09:49:30 -0400

    Update footer links
    Signed-off-by: Matthew H. Irby <matt.irby@outlook.com>

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -9,22 +9,9 @@
 
             <!-- Grid column for Social and Technology Icons -->
             <div class="col-md-12 mt-md-0 mt-3">
-                <div class="d-flex justify-content-around flex-wrap gap-3">
-                    <div>
-                        <!-- Content -->
-                        {{--                        <h4 class="mb-4">Join US</h4>--}}
+                <div class="d-flex justify-content-around flex-wrap flex-column flex-md-row gap-3">
 
-                        <div class="text-center footer-social-icons">
-                            <a href="/events" class="footer-social-icon-wrapper">
-                                <img src="{{url('img/icons/meetup.svg')}}" class="footer-social-icon" alt="Meetup"/>
-                            </a>
-                            <a href="/join-slack" class="footer-social-icon-wrapper ml-5">
-                                <img src="{{url('img/icons/slack.png')}}" class="footer-social-icon" alt="Slack"/>
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="d-flex flex-column align-middle h-100">
+                    <div class="d-flex flex-column align-middle h-100 order-1 order-md-1">
                         <h4 class="mb-4">Built with Laravel</h4>
 
                         <div class="text-center footer-technology-icons">
@@ -36,31 +23,82 @@
                     </div>
 
                     <!-- Grid column for Links -->
-                    <div>
+                    <div class="order-2 order-md-2 mt-4 mt-md-0">
                         <!-- Links -->
                         <h4 class="mb-4">Links</h4>
 
-                        <ul class="list-unstyled d-flex justify-content-around flex-wrap gap-2 flex-column">
+                        <ul class="list-unstyled justify-content-around flex-wrap gap-2 links-grid">
                             <li>
-                                <a class="@if(Route::is('calendar.*')) active @endif"
+                                <a
                                    href="{{route('calendar.index')}}">
                                     <i class="fa fa-calendar"></i>
                                     {{__('Calendar')}}
                                 </a>
                             </li>
                             <li>
-                                <a class="@if(Route::is('events.*')) active @endif"
+                                <a
                                    href="{{route('events.index')}}">
                                     <i class="fa fa-calendar-check-o"></i>
                                     {{__('Events')}}
                                 </a>
                             </li>
                             <li>
-                                <a class="@if(Route::is('join-slack.*')) active @endif" href="/join-slack">
+                                <a
+                                   href="{{route('orgs.index')}}">
+                                    <i class="fa fa-building"></i>
+                                    {{__('Organizations')}}
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                   href="{{route('labs.index')}}">
+                                    <i class="fa fa-flask"></i>
+                                    {{__('Labs')}}
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                   href="{{route('hg-nights.index')}}">
+                                    <i class="fa fa-moon-o"></i>
+                                    {{__('HG Nights')}}
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                   href="{{route('about')}}">
+                                    <i class="fa fa-users"></i>
+                                    {{__('About Us')}}
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                   href="{{route('contribute')}}">
+                                    <i class="fa fa-handshake-o"></i>
+                                    {{__('Contribute')}}
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                   href="{{route('contact')}}">
+                                    <i class="fa fa-paper-plane"></i>
+                                    {{__('Contact')}}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/join-slack">
                                     <i class="fa fa-slack"></i>
                                     {{__('Join Slack')}}
                                 </a>
                             </li>
+                        </ul>
+
+                    </div>
+
+                    <div class="order-3 order-md-3 mt-4 mt-md-0">
+                        <!-- Links -->
+                        <h4 class="mb-4">Other Links</h4>
+
+                        <ul class="list-unstyled d-flex justify-content-around flex-wrap gap-2 flex-column">
                             <li>
                                 <a class="@if(Route::is('code-of-conduct.*')) active @endif" href="{{route('code-of-conduct')}}">
                                     <i class="fa fa-check"></i>
