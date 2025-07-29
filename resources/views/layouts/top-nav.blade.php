1b1da2d87c82e4e00c81c763f3a0ab0c7f962bc3
david <beetz12@gmail.com>
2025-07-29 05:05:36 -0400

    fixed header links

--- resources/views/layouts/top-nav.blade.php
+++ resources/views/layouts/top-nav.blade.php
@@ -8,7 +8,7 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
                 <x-nav-link route="calendar.index" icon="fa-calendar">Calendar</x-nav-link>
