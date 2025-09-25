6fab2709020e12f0bffb397fe40ec7c5dde71721
bogdankharchenko <32746389+bogdankharchenko@users.noreply.github.com>
2025-09-25 03:21:55 +0000

    Fix styling

--- database/migrations/2025_09_24_232105_drop_users_table.php
+++ database/migrations/2025_09_24_232105_drop_users_table.php
@@ -1,16 +1,15 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-	public function up(): void
-	{
-		Schema::dropAllTables();
-	}
+    public function up(): void
+    {
+        Schema::dropAllTables();
+    }
 
-	public function down(): void
-	{
-	}
+    public function down(): void
+    {
+    }
 };
