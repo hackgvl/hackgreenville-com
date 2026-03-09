0b5567b3d952893aa1255366d13999c7070e069c
Bogdan Kharchenko <scriptor@gmail.com>
2026-03-09 17:30:48 -0400

    Add Railway deploy script and update yarn.lock dependencies

---
+++ scripts/railway-deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Install the JavaScript dependencies
+echo "Installing JavaScript dependencies..."
+yarn install --frozen-lockfile
+echo "JavaScript dependencies installation completed."
+
+# Run Laravel optimization commands
+echo "Configuring Laravel"
+php artisan icons:cache
+php artisan filament:cache-components
+php artisan cloudflare:cache:purge
+echo "Laravel optimization completed."
+
+# Bundle and optimize assets
+echo "Beginning Vite build..."
+yarn run build
+echo "Vite build completed! Assets have been bundled and/or optimized."
+
+# Finish
+echo "Deployment completed."
+
+chmod +x ./scripts/railway-cron.sh && ./scripts/railway-cron.sh
