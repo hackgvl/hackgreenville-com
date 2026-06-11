8d418c65a0708e4f1e0e860e1344bda7fa200fc2
Kevin Dees <kevin@robojuice.com>
2026-06-11 16:06:22 -0400

    Refine CTA heading spacing for improved vertical alignment consistency

--- resources/views/components/cta.blade.php
+++ resources/views/components/cta.blade.php
@@ -2,7 +2,7 @@
 
 <div {{ $attributes->class('flex flex-col sm:flex-row sm:items-center sm:justify-between gap-6') }}>
     <div class="max-w-xl">
-        <x-section-heading class="mb-8">{{ $title }}</x-section-heading>
+        <x-section-heading class="mb-4">{{ $title }}</x-section-heading>
         @if ($slot->isNotEmpty())
             <p class="text-base text-gray-600">{{ $slot }}</p>
         @endif
